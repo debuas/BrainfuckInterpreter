17873fc0e673fc0c15975c7f123b2753436517e4
debuas <debuas@localhost>
2021-06-16 12:31:41 +0200

    neue Datei:     .gitignore 	neue Datei:     .gradle/6.6.1/executionHistory/executionHistory.bin 	neue Datei:     .gradle/6.6.1/executionHistory/executionHistory.lock 	neue Datei:     .gradle/6.6.1/fileChanges/last-build.bin 	neue Datei:     .gradle/6.6.1/fileHashes/fileHashes.bin 	neue Datei:     .gradle/6.6.1/fileHashes/fileHashes.lock 	neue Datei:     .gradle/6.6.1/gc.properties 	neue Datei:     .gradle/buildOutputCleanup/buildOutputCleanup.lock 	neue Datei:     .gradle/buildOutputCleanup/cache.properties 	neue Datei:     .gradle/buildOutputCleanup/outputFiles.bin 	neue Datei:     .gradle/checksums/checksums.lock 	neue Datei:     .gradle/configuration-cache/gc.properties 	neue Datei:     .gradle/vcs-1/gc.properties 	neue Datei:     build.gradle.kts 	neue Datei:     gradle.properties 	neue Datei:     gradle/wrapper/gradle-wrapper.jar 	neue Datei:     gradle/wrapper/gradle-wrapper.properties 	neue Datei:     gradlew 	neue Datei:     gradlew.bat 	neue Datei:     settings.gradle.kts 	neue Datei:     src/main/kotlin/interpreter/Interpreter.kt 	neue Datei:     src/main/kotlin/virtualmachine/VirtualMachineInterface.kt 	neue Datei:     src/main/kotlin/virtualmachine/Virtualmachine.kt 	geändert:       src/main/kotlin/virtualmachine/VirtualMachineInterface.kt

---
+++ src/main/kotlin/virtualmachine/VirtualMachineInterface.kt
@@ -0,0 +1,22 @@
+package virtualmachine
+
+interface VirtualMachineInterface {
+
+
+    fun movePointerLeft()
+
+    fun movePointerRight();
+
+    fun incrementPointer();
+
+    fun decrementPointer();
+
+    fun printChar();
+
+    fun readChar();
+
+    fun loop()
+
+
+
+}
---
+++ src/main/kotlin/virtualmachine/Virtualmachine.kt
@@ -0,0 +1,34 @@
+package virtualmachine
+
+class Virtualmachine : VirtualMachineInterface {
+
+
+
+    override fun movePointerLeft() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun movePointerRight() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun incrementPointer() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun decrementPointer() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun printChar() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun readChar() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun loop() {
+        //TODO("Not yet implemented")
+    }
+}
